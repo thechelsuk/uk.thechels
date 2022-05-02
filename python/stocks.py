c07f538cf5bb03f54d32eae7bb7c7881769e6967
thechelsuk <github@thechels.uk>
2022-05-02 22:45:04 +0100

    Update stocks.py

--- python/stocks.py
+++ python/stocks.py
@@ -1,13 +1,12 @@
-# import
+# importin modules
 import helper
-import random
 import json
 import pathlib
 from yahoo_fin import stock_info as si
 
 root = pathlib.Path(__file__).parent.parent.resolve()
 with open( root / "config/stocks.json", 'r') as filehandle:
-  stocks_list = json.load(filehandle)
+    stocks_list = json.load( filehandle )
 
 
 def get_stocks(set_of_tickers):
@@ -15,6 +14,7 @@ def get_stocks(set_of_tickers):
     for ticker in list(set_of_tickers):
         string_builder += f"<li>{ticker} : {round(si.get_live_price(ticker),5)}</li>\n"
     return string_builder
+
 
 # output
 if __name__ == "__main__":
