64bfd2bb230978f71e2275d36726cd76d3f1d97e
thechelsuk <github@thechels.uk>
2022-05-02 22:39:57 +0100

    Update helper.py

--- python/helper.py
+++ python/helper.py
@@ -2,6 +2,7 @@
 import re
 import json
 
+
 # Replacer function
 def replace_chunk(content, marker, chunk):
     replacer = re.compile(
@@ -9,22 +10,24 @@ def replace_chunk(content, marker, chunk):
         re.DOTALL,
     )
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
-    return replacer.sub(chunk, content)
+    return replacer.sub(chunk, content) 
 
-
-def remove_img_tags(data):
-    p = re.compile(r'<img.*?/>')
-    return p.sub('', data)
 
 # Methods
+def remove_img_tags(data):
+    p = re.compile(r'<img.*?/>')
+    return p.sub('', data) 
+
+
 def ord(n):
-    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+    return str(n)+("th" if 4 <= n % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")) 
 
 
 def dtStylish(dt,f):
-    return dt.strftime(f).replace("{th}", ord(dt.day))
+    return dt.strftime(f).replace("{th}", ord(dt.day)) 
 
 
 def pprint(string):
     json_formatted_str = json.dumps(string, indent=2)
-    print(json_formatted_str)
+    print(json_formatted_str) 
+
