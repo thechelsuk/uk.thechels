f50cab7dc05bc19f02ea2796366bb8b71809ed69
thechelsuk <github@thechels.uk>
2022-05-02 23:37:37 +0100

    Update quotes.py

--- python/quotes.py
+++ python/quotes.py
@@ -8,12 +8,11 @@ import json
 root = pathlib.Path(__file__).parent.parent.resolve()
 with open( root / "config/quotes.json", 'r') as filehandle:
     random_quote = random.choice(json.load(filehandle))
-    random_quote = random_quote.replace( "-", "<br/> -")
+    random_quote = random_quote.replace( "-", "\n\n -")
 
 # processing
 if __name__ == "__main__":
-    index_page = root / "morning.md"
-    index_contents = index_page.open().read()
-    final_output = helper.replace_chunk(index_contents, "quote_marker", 
-                                        f"<blockquote>\n{random_quote}\n</blockquote>\n")
-    index_page.open("w").write(final_output)
+    p = root / "morning.md"
+    c = p.open().read()
+    f = helper.replace_chunk( c, "quote_marker", f"> {random_quote}")
+    p.open("w").write(f)
