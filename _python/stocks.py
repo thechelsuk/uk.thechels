8f81eb35deaa2fb6781082a5410703ee8889e3b7
thechelsuk <github@thechels.uk>
2022-05-03 21:44:08 +0100

    Update stocks.py

--- _python/stocks.py
+++ _python/stocks.py
@@ -19,7 +19,7 @@ def get_stocks(set_of_tickers):
 
 # output
 if __name__ == "__main__":
-    p = root / "morning.md"
+    p = root / "morning.html"
     c = p.open().read()
     s = get_stocks(stocks_list)
     f = helper.replace_chunk(c, "stocks_marker", f"<ul>\n{s}</ul>")
