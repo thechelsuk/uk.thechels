8ddd9b63ad5db58a6bf2a132447892fa247d7cae
thechelsuk <github@thechels.uk>
2022-05-03 13:47:55 +0100

    Update updater.py

--- _python/updater.py
+++ _python/updater.py
@@ -7,9 +7,8 @@ filename = os.getenv("label") or "test"
 content = os.getenv("content") or "content"
 
 root = pathlib.Path(__file__).parent.parent.resolve()
-with open( root / f"config/{filename}.json", 'r+') as filehandle:
+with open( root / f"_config/{filename}.json", 'r+') as filehandle:
     data = json.load(filehandle)
-    print(data)
     data.append(f"{content.rstrip()}")
     filehandle.seek(0)
     json.dump(data, filehandle, indent=4)
