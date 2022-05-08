a3881d7c8fb7de6d3eec4ca25ec12e3a9c177ad9
thechelsuk <github@thechels.uk>
2022-05-08 23:03:34 +0100

    Update updater.py

--- _python/updater.py
+++ _python/updater.py
@@ -3,8 +3,8 @@ import json
 import os
 import pathlib
 
-filename = os.getenv("label") or "test"
-content = os.getenv("content") or "content"
+filename = os.getenv("label")
+content = os.getenv("content")
 
 root = pathlib.Path(__file__).parent.parent.resolve()
 with open( root / f"_data/{filename}.json", 'r+') as filehandle:
