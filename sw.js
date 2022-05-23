56335cd4cf6d2b94aaeee60cc2b48a9037b53aef
thechelsuk <github@thechels.uk>
2022-05-23 20:14:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Incrementing CACHE_VERSION will kick off the install event and force previously cached
-// resources to be cached again.
 const CACHE_VERSION = 1;
 let CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
@@ -10,9 +8,6 @@ const OFFLINE_URL = 'about.html';
 
 function createCacheBustedRequest(url) {
   let request = new Request(url, {cache: 'reload'});
-  // See https://fetch.spec.whatwg.org/#concept-request-mode
-  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see
-  // if the cache: 'reload' option had any effect.
   if ('cache' in request) {
     return request;
   }
@@ -35,10 +30,25 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CURRENT_CACHES).then(function(cache) {
+      return cache.addAll(
+        [
+          '/style.css',
+          '/index.html',
+          '/images/gh-header-image-cropped.jpg',
+          '/about.html',
+          '/books.html',
+          '/podcasts.html',
+          '/morning.html'         
+        ]
+      );
+    })
+  );
+});
+
 self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
   let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
     return CURRENT_CACHES[key];
   });
@@ -60,30 +70,15 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  // request.mode of 'navigate' is unfortunately not supported in Chrome
-  // versions older than 49, so we need to include a less precise fallback,
-  // which checks for a GET request with an Accept: text/html header.
   if (event.request.mode === 'navigate' ||
       (event.request.method === 'GET' &&
        event.request.headers.get('accept').includes('text/html'))) {
     console.log('Handling fetch event for', event.request.url);
     event.respondWith(
       fetch(event.request).catch(error => {
-        // The catch is only triggered if fetch() throws an exception, which will most likely
-        // happen due to the server being unreachable.
-        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-        // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-        // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
         console.log('Fetch failed; returning offline page instead.', error);
         return caches.match(OFFLINE_URL);
       })
     );
   }
-
-  // If our if() condition is false, then this fetch handler won't intercept the request.
-  // If there are any other fetch handlers registered, they will get a chance to call
-  // event.respondWith(). If no fetch handlers call event.respondWith(), the request will be
-  // handled by the browser as if there were no service worker involvement.
 });
