e4d3f9c9b099db3cc66182508e564f51011f717c
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-01 16:51:10 +0100

    fixes
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/word.py
+++ _python/word.py
@@ -16,22 +16,17 @@ def replace_chunk(content, marker, chunk):
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
     return replacer.sub(chunk, content)
 
-def fetch_blog_entries():
-    entries = feedparser.parse("https://wordsmith.org/awad/rss1.xml")["entries"]
-    return [
-        {
-            "title": entry["title"],
-            "desc": entry["description"]
-        }
-        for entry in entries
-    ]
+item = feedparser.parse("https://wordsmith.org/awad/rss1.xml")['entries']
+title = item[0]["title"]
+desc = item[0]["summary"]
+string = f"\n > {title} - {desc} \n"
+
+print(string)
 
 if __name__ == "__main__":
     readme = root / "_pages/morning.md"
     readme_contents = readme.open().read()
-    entries = fetch_blog_entries()[:1]
-    word = "\n".join("####" + {title} + "\n > " + {desc} + "\n".format(**entry) for entry in entries)
-    rewritten = replace_chunk(readme_contents, "word", word)
+    rewritten = replace_chunk(readme_contents, "word", string)
     readme.open("w").write(rewritten)
 
     print("Word completed")
