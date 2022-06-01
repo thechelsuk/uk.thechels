8806d13431c8ca25dec77fd6c2d013e1ebdfb2f4
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-01 17:28:30 +0100

    tidying up
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/cfc.py
+++ _python/cfc.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     mdFile_contents = mdFile.open().read()
     entries = helper.fetch_cfc_entries()[:5]
     content = "\n".join(
-        ["- [{title}]({url}) - {published}".format(**entry) for entry in entries]
+        ["- [{title}]({url}) - {published}\n".format(**entry) for entry in entries]
     )
 
     rewritten = helper.replace_chunk(mdFile_contents, "cfc_marker", content)
