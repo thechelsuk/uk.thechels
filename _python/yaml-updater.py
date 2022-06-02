53d66b14339e9508c3f4d1ea3e45a105a5d8429f
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-02 01:47:07 +0100

    fix
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/yaml-updater.py
+++ _python/yaml-updater.py
@@ -20,13 +20,13 @@ if __name__ == "__main__":
     c = args.content
     l = args.label
 
-root = pathlib.Path(__file__).parent.parent.resolve()
-c = c.replace('`','').strip()
-l = l.strip()
-p = root / f"_data/{l}.yml"
-try:
-    with open(p, 'a') as f:
-        o = f.write(f"- {c}")
-        print(f"{l} completed\n{o}")
-except FileNotFoundError:
-        print('File does not exist, unable to proceed')
+    root = pathlib.Path(__file__).parent.parent.resolve()
+    c = c.replace('`','').strip()
+    l = l.strip()
+    p = root / f"_data/{l}.yml"
+    try:
+        with open(p, 'a') as f:
+            o = f.write(f"- {c}")
+            print(f"{l} completed\n{o}")
+    except FileNotFoundError:
+            print('File does not exist, unable to proceed')
