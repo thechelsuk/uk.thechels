3d41cb2cba28e874497e28cd7266a528533e1e04
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-02 01:46:29 +0100

    feat/ add argparse to remove error
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/yaml-updater.py
+++ _python/yaml-updater.py
@@ -1,10 +1,28 @@
 # importing modules
-import os
 import pathlib
+import argparse
+
+if __name__ == "__main__":
+    arguments = argparse.ArgumentParser(
+        description='''Get content and labels to process file updates.'''
+    )
+
+    arguments.add_argument('--content',
+        type=str,
+        help='The body of text to use',
+        required=True)
+    arguments.add_argument('--label',
+        type=str,
+        help='The label gives us the file context',
+        required=True)
+
+    args = arguments.parse_args()
+    c = args.content
+    l = args.label
 
 root = pathlib.Path(__file__).parent.parent.resolve()
-c = os.getenv("content").replace('`','').strip()
-l = os.getenv("label").strip()
+c = c.replace('`','').strip()
+l = l.strip()
 p = root / f"_data/{l}.yml"
 try:
     with open(p, 'a') as f:
