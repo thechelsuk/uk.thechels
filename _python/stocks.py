514a157b474f6daabfa43440b3a9fadc794f62c5
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-02 02:11:27 +0100

    fix/ typo
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/stocks.py
+++ _python/stocks.py
@@ -5,7 +5,7 @@ import helper
 
 
 root = pathlib.Path(__file__).parent.parent.resolve()
-with open( root / "_data/stocks.yaml", 'r') as stream:
+with open( root / "_data/stocks.yml", 'r') as stream:
     stocks_list = yaml.load(stream, Loader=yaml.FullLoader)
 
 
