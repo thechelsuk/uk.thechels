0bc92d2791d647f08c5fa4be92d600e39ff28190
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-05 20:21:45 +0100

    debugging
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/yaml-updater.py
+++ _python/yaml-updater.py
@@ -17,20 +17,18 @@ if __name__ == "__main__":
         required=True)
 
     args = arguments.parse_args()
-    c = args.content
+    content = args.content
     l = args.label
 
     root = pathlib.Path(__file__).parent.parent.resolve()
-    c = c.replace('`','').strip()
-    l = l.strip()
-    p = root / f"_data/{l}.yml"
-
-    print(c)
-    print(l)
+    print(f"input: {content}")
+    c = content.replace('`','')
+    print(f"Replaced {c}")
+    p = root / f"_data/{l.strip()}.yml"
 
     try:
         with open(p, 'a') as f:
-            o = f.write(f"- {c}")
+            o = f.write(f"- {c.strip()}")
             print(f"{l} completed\n{o}")
     except FileNotFoundError:
             print('File does not exist, unable to proceed')
