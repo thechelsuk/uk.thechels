64866bf9b33b1bdbe403cac45ec1cafce22a4c69
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-05 21:12:26 +0100

    fix
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/podcasts.py
+++ _python/podcasts.py
@@ -4,7 +4,7 @@ import pathlib
 
 root = pathlib.Path(__file__).parent.parent.resolve()
 yamlFile = root / "_data/podcasts.yml"
-content = os.getenv("content").replace('`','').strip()
+content = os.getenv("content").replace('`','').strip() + "/n"
 
 try:
     with open(yamlFile, 'a') as f:
