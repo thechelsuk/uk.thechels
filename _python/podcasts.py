b8e3179d2a679758002c774379b4ef05e8c42ab1
thechelsuk <github@thechels.uk>
2022-06-07 19:52:16 +0100

    Update podcasts.py

--- _python/podcasts.py
+++ _python/podcasts.py
@@ -4,11 +4,12 @@ import pathlib
 
 root = pathlib.Path(__file__).parent.parent.resolve()
 yamlFile = root / "_data/podcasts.yml"
-content = os.getenv("content").replace('`','').strip() + "/n"
+content = os.getenv("content").replace('`','').strip()
 
 try:
     with open(yamlFile, 'a') as f:
-        outputs = f.write(content)
+        f.rstrip('/n')
+        outputs = f.write(content  + '/n' )
         print(outputs)
 except FileNotFoundError:
         print('File does not exist, unable to proceed')
