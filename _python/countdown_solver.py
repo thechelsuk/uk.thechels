0b3c96f5d4a872a979a9e700dbf682a3158dce77
MatBenfield <MatBenfield@users.noreply.github.com>
2022-06-15 20:58:49 +0100

    comment
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- _python/countdown_solver.py
+++ _python/countdown_solver.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Modified by MatBenfield to support the addition return solution and summary as text strings
 
 OPTIMIZE = 1
 
