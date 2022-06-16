bf28b945c03717b88526b70784847ac881c48cb3
thechelsuk <github@thechels.uk>
2022-06-16 21:41:21 +0100

    Update about.md (#352)
    first commit
    
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>
    Co-authored-by: MatBenfield <MatBenfield@users.noreply.github.com>

---
+++ _python/block_updater.py
@@ -0,0 +1,16 @@
+# import modules
+import os
+import pathlib
+
+# processing
+try:
+    label = os.getenv("label")
+    content = os.getenv("content").replace("`", "").strip()
+    root = pathlib.Path(__file__).parent.parent.resolve()
+    yaml_file = root / "_data/{label}.yml"
+    with open(yaml_file, "a") as f:
+        outputs = f.write("/n" + content)
+    print(f"{label} completed")
+
+except FileNotFoundError:
+    print(f"File does not exist for matching label {label}, unable to proceed")
