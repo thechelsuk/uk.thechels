7039c79f50505345a82882a9b09c2a0b8a726b97
thechelsuk <github@thechels.uk>
2022-07-11 07:46:10 +0100

    Update countdown.py

--- _python/countdown.py
+++ _python/countdown.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
         target = random.randint(100, 1000)
         selected = helper.get_countdown_number_selection()
         countdown_solver.OPTIMIZE = 1
-        (list, summary) = countdown_solver.solve(selected, target)
+        (answers_list, summary) = countdown_solver.solve(selected, target)
         string = f"- Target: {target}, using {selected}\n"
-        string += f"- Solution: {list[0]}\n"
-        string += f"- {summary}\n"
+        if answers_list:
+            string += f"- Solution: {answers[0]}\n"
+            string += f"- {summary}\n"
         f = root / "_pages/morning.md"
         m = f.open().read()
         c = helper.replace_chunk(m, "game_marker", f"\n{string}")
