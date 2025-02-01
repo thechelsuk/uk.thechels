3199fb99093e5a952a67e5e0e69c63d48dc80f40
thechelsuk <github@thechels.uk>
2025-02-01 15:40:02 +0000

    derp
    Signed-off-by: mat-0 <Github@thechels.uk>

--- _python/stocks.py
+++ _python/stocks.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         root = pathlib.Path(__file__).parent.parent.resolve()
         with open(root / "_data/stocks.yml", "r") as stream:
             stocks_list = yaml.load(stream, Loader=yaml.FullLoader)
-        f = root / "_pages/morning.md"
+        f = root / "_pages/daily.md"
         m = f.open().read()
         s = helper.get_stocks(stocks_list)
         c = helper.replace_chunk(m, "stocks_marker", f"\n{s}")
