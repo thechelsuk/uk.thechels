5e78db9def91571fdc31fa68fc486acc36f8b410
GitHub Action <action@github.com>
2025-02-14 10:05:55 +0000

    Python format

--- _python/ftnews.py
+++ _python/ftnews.py
@@ -9,10 +9,8 @@ if __name__ == "__main__":
     try:
         root = pathlib.Path(__file__).parent.parent.resolve()
 
-        nf = feedparser.parse(
-            "https://www.ft.com/?format=rss")["entries"]
-        nh = "\n".join(
-            [f" - {entry['title']}" for entry in nf[5]])
+        nf = feedparser.parse("https://www.ft.com/?format=rss")["entries"]
+        nh = "\n".join([f" - {entry['title']}" for entry in nf[5]])
         string = f"\n{nh}\n"
         f = root / "_pages/daily.md"
         m = f.open().read()
