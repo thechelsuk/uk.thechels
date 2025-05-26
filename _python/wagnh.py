543da4c73ac1d58140e83a0c8c457a595997dc88
GitHub Action <action@github.com>
2025-05-26 13:34:30 +0000

    Python format

--- _python/wagnh.py
+++ _python/wagnh.py
@@ -5,7 +5,8 @@ import helper
 if __name__ == "__main__":
     root = pathlib.Path(__file__).parent.parent.resolve()
     OUTPUT_FILE = root / "_pages/daily.md"
-    nf = feedparser.parse("https://weaintgotnohistory.sbnation.com/rss/current.xml")["entries"]
+    nf = feedparser.parse(
+        "https://weaintgotnohistory.sbnation.com/rss/current.xml")["entries"]
     nh = "\n".join([f"- {entry['title']}" for entry in nf[:10]])
     string = f"\n{nh}\n"
     KEY = "news_marker"
