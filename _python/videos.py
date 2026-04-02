c4fe671a18e7cd24b79a11d531b02d17633405bb
GitHub Action <action@github.com>
2026-04-02 00:02:30 +0000

    Python format

--- _python/videos.py
+++ _python/videos.py
@@ -4,13 +4,13 @@ import helper
 SOURCE = "https://www.youtube.com/feeds/videos.xml?channel_id=UCwK4oZ8hw9RS6tZKEjw_qLw"
 OUTPUT_FILE = "./_data/videos.yml"
 
-
 if __name__ == "__main__":
     try:
         root = pathlib.Path(__file__).parent.parent.resolve()
         output_file = root / OUTPUT_FILE
         existing_videos = helper.load_videos_file(output_file)
-        merged_videos = helper.merge_videos(existing_videos, helper.get_videos(SOURCE))
+        merged_videos = helper.merge_videos(existing_videos,
+                                            helper.get_videos(SOURCE))
         helper.save_videos(output_file, merged_videos)
         print("Videos completed")
 
